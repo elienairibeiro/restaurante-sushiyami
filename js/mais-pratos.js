2f950fcfea2905db5f8e554a97d32d36705436f7
elienairibeiro <elienairibeiro.rc@gmail.com>
2019-04-30 16:00:44 -0300

    subindo

---
+++ js/mais-pratos.js
@@ -0,0 +1,12 @@
+document.querySelector("#mais").addEventListener('click', function(){
+    $('#prato-11').css('display','inline-block');
+    $('#prato-12').css('display','inline-block');
+    $('#prato-13').css('display','inline-block');
+    $('#prato-14').css('display','inline-block');
+    $('#prato-15').css('display','inline-block');
+    $('#prato-16').css('display','inline-block');
+    $('#prato-17').css('display','inline-block');
+    $('#prato-18').css('display','inline-block');
+    $('#prato-19').css('display','inline-block');
+    $('#prato-20').css('display','inline-block');
+})
